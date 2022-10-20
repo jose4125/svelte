cdfe9a9fab1023f38bec537f369e4ef8e004d219
jose lombana <jose.lombana@optimizely.com>
2022-10-19 20:02:52 -0500

    modularizando componentes

--- apps/weather-app-ts/src/weather/view/Weather.svelte
+++ apps/weather-app-ts/src/weather/view/Weather.svelte
@@ -47,47 +47,18 @@
       <label>
         city:<input class="form-control" bind:value={city} />
       </label>
-      <Button {isLoading}>
+      <Button type="submit" {isLoading} buttonType="btn-info">
         {isLoading ? 'searching...' : 'search'}
       </Button>
     </form>
   </div>
   {#if isLoading}
-    <Spinner />
+    <div class="d-flex justify-content-center">
+      <Spinner textStyle="text-info" />
+    </div>
   {:else}
     <div class="row mt-4">
       <WeatherDetail data={$store} />
-      <!-- <div class="col-12 col-md-6">
-        <h1>
-          <img
-            class="bg-secondary"
-            src={$store.weather.weatherIcon}
-            alt={$store.weather.city}
-          />
-          {$store.weather.degrees}°C
-        </h1>
-        <h4>
-          City:
-          <span class="fw-bold">
-            {$store.weather.city}, {$store.weather.country}
-          </span>
-        </h4>
-        <h4>
-          Weather: <span class="fw-bold">{$store.weather.description}</span>
-        </h4>
-      </div>
-      <div class="col-12 col-md-6 ">
-        <h4>
-          Min Temp: <span class="fw-bold text-primary"
-            >{$store.weather.minDegrees}°C</span
-          >
-        </h4>
-        <h4>
-          Max Temp: <span class="fw-bold text-danger"
-            >{$store.weather.maxDegrees}°C</span
-          >
-        </h4>
-      </div> -->
     </div>
     {#if error}
       <Alert message={error} />
